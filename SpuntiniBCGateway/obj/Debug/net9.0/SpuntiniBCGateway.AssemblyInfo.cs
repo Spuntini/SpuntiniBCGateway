2e58cdb697f4a5a80e213a33d7b01b4e7383cf96
Lode Vander Stichele <lode.vanderstichele@spuntinigroup.be>
2026-01-12 08:56:27 +0100

    Update 12/01

--- SpuntiniBCGateway/obj/Debug/net9.0/SpuntiniBCGateway.AssemblyInfo.cs
+++ SpuntiniBCGateway/obj/Debug/net9.0/SpuntiniBCGateway.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SpuntiniBCGateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79fbe38326afdee27a46522e82e82be212754d65")]
 [assembly: System.Reflection.AssemblyProductAttribute("SpuntiniBCGateway")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SpuntiniBCGateway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
